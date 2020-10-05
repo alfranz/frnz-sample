bd4e77607cb051b9771d54645622d27606021031
Alexander Franz <alexfranz90@googlemail.com>
2020-10-05 07:36:56 +0200

    new capetown entry in dataframe

--- frnz/data.py
+++ frnz/data.py
@@ -2,8 +2,16 @@ import pandas as pd
 
 cities = pd.DataFrame(
     data={
-        "city": ["Berlin", "Vienna", "Montreal", "Mumbai", "Cape Town", "Sydney"],
-        "country": ["DE", "AT", "CA", "IN", pd.NA, "AUS"],
-        "population": [3750000, 1900000, 1780000, 184100000, 430000, pd.NA],
+        "city": [
+            "Berlin",
+            "Vienna",
+            "Montreal",
+            "Mumbai",
+            "cape-town",
+            "Cape Town",
+            "Sydney",
+        ],
+        "country": ["DE", "AT", "CA", "IN", pd.NA, "ZA", "AUS"],
+        "population": [3750000, 1900000, 1780000, 184100000, 430000, 440000, pd.NA],
     }
 )
